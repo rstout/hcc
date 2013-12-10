ea30a1c248cfd544ecf45fa9856b2a37b8b61007
Ryan Stout <rstout@andrew.cmu.edu>
2013-12-10 16:53:07 -0500

    AAsm3Op code complete

--- L1/compiler/src/Compile/IR.hs
+++ L1/compiler/src/Compile/IR.hs
@@ -1,13 +1,14 @@
-{-| IR.hs 
-    Author: Ryan Stout 
+{-| IR.hs
+    Author: Ryan Stout
 
     Generates intermediate representations
 -}
 
 module Compile.IR where
-       
+
 import Compile.Types
 import Compile.IR.CodeGen
-       
+import Compile.IR.AAsm3Op
+
 genIR :: AST -> IR
 genIR = undefined
--- L1/compiler/src/Compile/Types.hs
+++ L1/compiler/src/Compile/Types.hs
@@ -2,7 +2,7 @@
    Author: Matthew Maurer <mmaurer@andrew.cmu.edu>
    Modified by: Ryan Pearl <rpearl@andrew.cmu.edu>
 -}
-module Compile.Types 
+module Compile.Types
        ( module X
        , SourceCode(..)
        , TargetCode
@@ -14,6 +14,7 @@ import Compile.Types.Job as X
 import Compile.Types.AbstractAssembly as X
 import Compile.Types.Ops as X
 import Compile.Types.AST as X
+import Compile.Types.AAsm as X
 
 import Data.ByteString (ByteString)
 
@@ -25,7 +26,7 @@ data IR = IR String -- placeholder
 {-
 class AST a where
   genIR :: (IR b) => a -> b
-           
+
 class IR a where
   genTargetCode :: a -> TargetCode
--} 
+-}
